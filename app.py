ffd14c2e729ab53198f023d199c1b058ee1379bc
Eric Cai <ericcai32@gmail.com>
2024-05-06 09:49:09 -0400

    Added the beginnings of a Flask server.

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, send_file, request, redirect, render_template, Blueprint, jsonify, make_response
+import redis
+import sqlite3
+from uuid import uuid4, UUID
+
+app = Flask(__name__)
+
+@app.route('/', methods=('GET', 'POST'))
+def index():
+    return "[HOME PAGE]"
+
+app.run(port=8022)
