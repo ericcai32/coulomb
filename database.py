4b676136751a533798ec7a6d14f272076faf2f75
slugle4649 <hugha.glenn@gmail.com>
2024-05-13 12:56:15 -0400

    database code

--- database.py
+++ database.py
@@ -1,4 +1,7 @@
 import sqlite3
+import random
+import string
+import hashlib
 
 con = sqlite3.connect('users.db')
 cur = con.cursor() 
@@ -25,13 +28,14 @@ def read_table(tournament_name):
         return False
     con = sqlite3.connect('users.db')
     cur=con.cursor()
-    cur.execute('SELECT * FROM ?', (tournament_name, ))
+    cur.execute(f'SELECT * FROM {tournament_name}')
     data = cur.fetchall()
     con.close()
     return data
 
 def add_to_table(tournament_name, school_name, data):
     if check_exists(tournament_name):
+        print('uhhhh')
         return False
     event_names = []
     scores = []
@@ -40,15 +44,92 @@ def add_to_table(tournament_name, school_name, data):
         scores.append(data[event])
     con = sqlite3.connect('users.db')
     cur = con.cursor()
-    cur.execute('INSERT INTO ? (Team) VALUES (?)', (tournament_name, school_name))
+    cur.execute(f'INSERT INTO {tournament_name} (Team) VALUES (?)', (school_name, ))
+    cur.execute(f'SELECT schools FROM tournaments WHERE name=?', (tournament_name, ))
+    schools = str(cur.fetchall()[0][0]).split()
+    print(schools)
+    if schools[0] == 'None':
+        schools = []
+    if school_name not in schools:
+        schools.append(school_name)
+
+    print(schools)
+    schools = ' '.join(map(str, schools))
+    print(schools)
+    cur.execute(f'UPDATE tournaments SET schools=? WHERE name=?', (schools, tournament_name))
     for i in range(len(scores)):
-        cur.execute(f'UPDATE TABLE {tournament_name} SET ? = ? WHERE Team = ?', (event_names[i], scores[i], school_name))
+        cur.execute(f'UPDATE {tournament_name} SET {event_names[i]} = {scores[i]} WHERE Team = ?', (school_name, ))
     con.commit()
     con.close()
     return True
 
-def register():
-    return
+def register(username, password):
+    con = sqlite3.connect('users.db')
+    cur = con.cursor()
+    cur.execute("SELECT name FROM accounts where name=?", (username,))
+    if not cur.fetchall():
+        hash = hashlib.sha256()
+        source = string.ascii_letters + string.digits
+        salt = ''.join((random.choice(source) for i in range(8)))
+        password += salt
+        hash.update(password.encode())
+        passwordHash = hash.hexdigest()
+        cur.execute('INSERT INTO accounts (name, password, salt) VALUES (?, ?, ?)', (username, passwordHash, salt))
+        con.commit()
+        con.close()
+        return True
+    con.close()
+    return False
+
+def login(username, password):
+    con = sqlite3.connect('users.db')
+    cur = con.cursor()
+    cur.execute("SELECT name FROM accounts where name=?", (username,))
+    if not cur.fetchall():
+        return False
+    cur.execute('SELECT password FROM accounts WHERE name=?', (username, ))
+    hashed_password = cur.fetchall()[0][0]
+    cur.execute('SELECT salt FROM accounts WHERE name=?', (username, ))
+    salt = cur.fetchall()[0][0]
+    password += salt
+    hash = hashlib.sha256()
+    hash.update(password.encode())
+    passwordHash = hash.hexdigest()
+    con.close()
+    if passwordHash == hashed_password:
+        return True
+    else:
+        return False
+
+def verify_creator(name, tourny_name):
+    con = sqlite3.connect('users.db')
+    cur = con.cursor()
+    cur.execute('SELECT creator FROM tournaments WHERE name=?', (tourny_name, ))
+    data = cur.fetchall()
+    con.close()
+    try:
+        data[0][0]
+    except:
+        return False
+    data = data[0][0]
+    return data == name
+
+def update_row(tourny_name, team, data):
+    con = sqlite3.connect('users.db')
+    cur = con.cursor()
+    cur.execute('SELECT name FROM tournaments WHERE name=?', (tourny_name, ))
+    if not cur.fetchall():
+        return False
+    event_names = []
+    scores = []
+    for event in data:
+        event_names.append(event)
+        scores.append(data[event])
+    for i in range(len(scores)):
+        cur.execute(f'UPDATE {tourny_name} SET {event_names[i]} = {scores[i]} WHERE Team = ?', (team, ))
+    con.commit()
+    con.close()
+    return True
 
 def check_exists(tournament_name):
     con = sqlite3.connect('users.db')
@@ -58,5 +139,31 @@ def check_exists(tournament_name):
     con.close()
     return len(data) == 0
 
+def get_all_tournaments():
+    rtn = []
+    con = sqlite3.connect('users.db')
+    cur = con.cursor()
+    cur.execute('SELECT name FROM tournaments')
+    data = cur.fetchall()
+    con.close()
+    for name in data:
+        rtn.append(name[0])
+    return rtn
 
+def get_participated_events():
+    con = sqlite3.connect('users.db')
+    cur = con.cursor()
+    cur.execute('SELECT')
 
+'''
+con = sqlite3.connect('users.db')
+cur = con.cursor()
+cur.execute('SELECT name FROM tournaments')
+tourny_names = []
+data = cur.fetchall()
+for name in data:
+    tourny_names.append(name[0])
+for tourny in tourny_names:
+    cur.execute('SELECT schools FROM ')
+con.close()
+'''
