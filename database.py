1e1be8924eae8dbc0624dcdaf3c6fd534cb0745f
slugle4649 <hugha.glenn@gmail.com>
2024-05-13 20:41:51 -0400

    placement function

--- database.py
+++ database.py
@@ -5,7 +5,7 @@ import hashlib
 
 con = sqlite3.connect('users.db')
 cur = con.cursor() 
-cur.execute('CREATE TABLE IF NOT EXISTS tournaments (name TEXT, creator TEXT);')
+cur.execute('CREATE TABLE IF NOT EXISTS tournaments (name TEXT, creator TEXT, schools TEXT);')
 cur.execute('CREATE TABLE IF NOT EXISTS accounts (id INTEGER PRIMARY KEY AUTOINCREMENT, name STRING, password STRING, salt STRING)')    
 con.commit()
 con.close()
@@ -47,15 +47,12 @@ def add_to_table(tournament_name, school_name, data):
     cur.execute(f'INSERT INTO {tournament_name} (Team) VALUES (?)', (school_name, ))
     cur.execute(f'SELECT schools FROM tournaments WHERE name=?', (tournament_name, ))
     schools = str(cur.fetchall()[0][0]).split()
-    print(schools)
     if schools[0] == 'None':
         schools = []
     if school_name not in schools:
         schools.append(school_name)
 
-    print(schools)
     schools = ' '.join(map(str, schools))
-    print(schools)
     cur.execute(f'UPDATE tournaments SET schools=? WHERE name=?', (schools, tournament_name))
     for i in range(len(scores)):
         cur.execute(f'UPDATE {tournament_name} SET {event_names[i]} = {scores[i]} WHERE Team = ?', (school_name, ))
@@ -171,10 +168,27 @@ def get_events(tourny_name):
     rtn = []
     con = sqlite3.connect('users.db')
     cur = con.cursor()
-    cur.execute('PRAGMA table_info(test_tournament_4)')
+    cur.execute(f'PRAGMA table_info({tourny_name})')
     data = cur.fetchall()
     for i in range(2, len(data)):
         rtn.append(data[i][1])
     con.close()
     return rtn
 
+def get_placements(team_name):
+    rtn = {}
+    con = sqlite3.connect('users.db')
+    cur = con.cursor()
+    data = get_participated_events(team_name)
+    for event in data:
+        temp_list = []
+        cur.execute(f'SELECT * FROM {event} WHERE Team=?', (team_name, ))
+        data = cur.fetchall()[0]
+        for i in range(2, len(data)):
+            temp_list.append(data[i])
+        rtn[event] = temp_list
+    con.close()
+    print(rtn)
+    return rtn
+
+get_placements('NCSSM')
