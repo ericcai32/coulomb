17ef6399cd4745c9c2b3a0bb3f67adf3a65e7b61
slugle4649 <hugha.glenn@gmail.com>
2024-05-17 16:30:00 -0400

    commented code on database.py

--- database.py
+++ database.py
@@ -3,6 +3,7 @@ import random
 import string
 import hashlib
 
+#Creates database and tables for initialization
 con = sqlite3.connect('users.db')
 cur = con.cursor() 
 cur.execute('CREATE TABLE IF NOT EXISTS tournaments (name TEXT, creator TEXT, schools TEXT);')
@@ -16,7 +17,14 @@ cur.execute('CREATE TABLE IF NOT EXISTS teams (team_name TEXT, tournament_name T
 con.commit()
 con.close()
 
-def create_tournament(tournament_name, events, creator) -> str:
+def create_tournament(tournament_name: str, events: list, creator: str) -> str:
+    '''
+    Creates a new table for a tournament
+    Params:
+        tournament_name: name of tournament
+        events: list of all events
+        creator: user who created the event
+    '''
     if check_exists(tournament_name) == False:
         return "A tournament with that name already exists."
     if len(events) != len(set(events)): # Check if the event list has any duplicates.
@@ -32,7 +40,15 @@ def create_tournament(tournament_name, events, creator) -> str:
     con.close()
     return ""
 
-def read_table(tournament_name):
+def read_table(tournament_name: str) -> list:
+    '''
+    Returns a table
+    Params:
+        tournament_name: Name of tournamet
+    
+    Returns:
+        List of tuples which is the table of the tournament
+    '''
     if check_exists(tournament_name):
         return False
     con = sqlite3.connect('users.db')
@@ -42,9 +58,18 @@ def read_table(tournament_name):
     con.close()
     return data
 
-def add_to_table(tournament_name, school_name, data):
+def add_to_table(tournament_name: str, school_name: str, data: dict) -> bool:
+    '''
+    Add data to table
+    Params:
+        tournament_name: Name of tournament
+        school_name: name of school of team
+        data: Dictionary where key is the event name, and the value is the placement/score for that event
+    
+    Returns:
+        Boolean if the addition to the table was a success
+    '''
     if check_exists(tournament_name):
-        print('uhhhh')
         return False
     event_names = []
     scores = []
@@ -70,7 +95,16 @@ def add_to_table(tournament_name, school_name, data):
     print('adding')
     return True
 
-def register(username, password):
+def register(username: str, password: str) -> bool:
+    '''
+    Register a new account
+    Params:
+        username: username of user
+        password: password of user
+    
+    Returns: 
+        Boolean if registering account was successful
+    '''
     con = sqlite3.connect('users.db')
     cur = con.cursor()
     cur.execute("SELECT name FROM accounts where name=?", (username,))
@@ -88,7 +122,16 @@ def register(username, password):
     con.close()
     return False
 
-def login(username, password):
+def login(username: str, password: str) -> bool:
+    '''
+    Logins user
+    Params: 
+        username: username of user
+        password: password of user
+    
+    Returns:
+        Boolean if the login was successful
+    '''
     con = sqlite3.connect('users.db')
     cur = con.cursor()
     cur.execute("SELECT name FROM accounts where name=?", (username,))
@@ -108,7 +151,16 @@ def login(username, password):
     else:
         return False
 
-def verify_creator(name, tourny_name):
+def verify_creator(name:str, tourny_name: str) -> bool:
+    '''
+    Verify that the person trying to access the tournament is the creator or not
+    Params:
+        name: name of the user
+        tourny_name: name of the tournament
+
+    Return:
+        boolean of if the user is the creator of the tournament or not
+    '''
     con = sqlite3.connect('users.db')
     cur = con.cursor()
     cur.execute('SELECT creator FROM tournaments WHERE name=?', (tourny_name, ))
@@ -121,7 +173,16 @@ def verify_creator(name, tourny_name):
     data = data[0][0]
     return data == name
 
-def update_row(tourny_name, team, data):
+def update_row(tourny_name: str, team: str, data: dict) -> bool:
+    '''
+    Allows the user to change tournament data if initial entry was incorrect
+    Params: 
+        tourny_name: name of tournament
+        team: name of team
+        data: dictionary where key is the event name, and value is the new placement for that event
+    Return:
+        Boolean on the change was succesful
+    '''
     con = sqlite3.connect('users.db')
     cur = con.cursor()
     cur.execute('SELECT name FROM tournaments WHERE name=?', (tourny_name, ))
@@ -138,7 +199,15 @@ def update_row(tourny_name, team, data):
     con.close()
     return True
 
-def check_exists(tournament_name):
+def check_exists(tournament_name: str) -> bool:
+    '''
+    Check to see if the tournament exists
+    Param:
+        tournament_name: name of tournament
+
+    Return:
+        boolean if the tournament exists or not
+    '''
     con = sqlite3.connect('users.db')
     cur = con.cursor()
     cur.execute('SELECT * FROM tournaments WHERE name = ?', (tournament_name, ))
@@ -146,7 +215,12 @@ def check_exists(tournament_name):
     con.close()
     return len(data) == 0
 
-def get_all_tournaments():
+def get_all_tournaments() -> list:
+    '''
+    Get all the tournaments
+    Returns:
+        Returns a list of all tournaments
+    '''
     rtn = []
     con = sqlite3.connect('users.db')
     cur = con.cursor()
@@ -157,7 +231,15 @@ def get_all_tournaments():
         rtn.append(name[0])
     return rtn
 
-def get_participated_events(school_name):
+def get_participated_events(school_name: str) -> list:
+    '''
+    Gets all the tournaments that the team/school participated in
+    Params:
+        school_name: name of team/school
+
+    Return:
+        List of all participated tournaments
+    '''
     rtn = []
     con = sqlite3.connect('users.db')
     cur = con.cursor()
@@ -174,7 +256,15 @@ def get_participated_events(school_name):
     con.close()
     return rtn
 
-def get_events(tourny_name):
+def get_events(tourny_name: str) -> list:
+    '''
+    Get all the events in a tournament
+    Params:
+        tourny_name: name of tournament
+
+    Return: 
+        List of all events in tournament
+    '''
     rtn = []
     con = sqlite3.connect('users.db')
     cur = con.cursor()
@@ -185,7 +275,15 @@ def get_events(tourny_name):
     con.close()
     return rtn
 
-def get_placements(team_name):
+def get_placements(team_name: str) -> dict:
+    '''
+    Get the placements for a team for every tournament
+    Params:
+        team_name: name of team
+
+    Return:
+        dictionary where key is the tournament name, and the value is a list of placements for that tournament
+    '''
     rtn = {}
     con = sqlite3.connect('users.db')
     cur = con.cursor()
@@ -201,7 +299,19 @@ def get_placements(team_name):
     print(rtn)
     return rtn
 
-def add_participant(team_name, tournament_name, participant_name, placement, event_name):
+def add_participant(team_name: str, tournament_name: str, participant_name: str, placement: str, event_name: str) -> bool:
+    '''
+    Add a participant to a team
+    Params:
+        team_name: name of team
+        tournamet_name: name of tournament
+        participant_name: name of participant
+        placement: placement of participant
+        event_name: name of event
+    
+    Return:
+        boolean if adding the participant was successful or not
+    '''
     con = sqlite3.connect('School.db')
     cur = con.cursor()
     cur.execute('SELECT placement FROM teams WHERE team_name=? AND tournament_name=? AND participant_name=? AND event_name=?', (team_name, tournament_name, participant_name, event_name))
@@ -214,7 +324,17 @@ def add_participant(team_name, tournament_name, participant_name, placement, eve
     con.close()
     return False
 
-def get_participant_data(team_name, tournament_name, participant_name):
+def get_participant_data(team_name: str, tournament_name: str, participant_name: str) -> list:
+    '''
+    Get data for a participant
+    Params:
+        team_name:name of team
+        tournament_name: name of tournament
+        participant_name: name of participant
+
+    Return:
+        List of lists where there is event name and placement for every list inside
+    '''
     con = sqlite3.connect('School.db')
     cur = con.cursor()
     cur.execute('SELECT event_name, placement FROM teams WHERE team_name=? AND tournament_name=? AND participant_name=?', (team_name, tournament_name, participant_name))
@@ -222,7 +342,15 @@ def get_participant_data(team_name, tournament_name, participant_name):
     con.close()
     return data
 
-def get_all_participants(team_name):
+def get_all_participants(team_name: str) -> list:
+    '''
+    Gets all participants in a team
+    Params:
+        team_name: name of team
+
+    Return:
+        List of all participant on team
+    '''
     rtn = []
     con = sqlite3.connect('School.db')
     cur = con.cursor()
